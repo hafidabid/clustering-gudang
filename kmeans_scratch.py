4f47733e6dd4b29e057ee1eb9d6fda6d15340a07
hafidabid <aselole87@gmail.com>
2022-11-07 02:19:42 +0700

    custom vincenty with pembobotan

--- kmeans_scratch.py
+++ kmeans_scratch.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 import random
 from vincenty import vincenty
@@ -6,11 +7,11 @@ class KMeansAlgorithm:
     def __init__(self, df: pd.DataFrame, K:int, lat_column, long_column, weight_factor=None):
         self.K = K
         self.df = df
-        self.lat_data = df[[lat_column]].tolist()
-        self.long_data = df[[long_column]].tolist()
+        self.lat_data = df[lat_column].tolist()
+        self.long_data = df[long_column].tolist()
         self.n_population = len(df)
         self.classification = [-1 for _ in range(self.n_population)]
-        self.weight_factor = weight_factor if not None else [1 for _ in range(self.n_population)]
+        self.weight_factor = weight_factor if weight_factor is not None else [1 for _ in range(self.n_population)]
         self.wcss = 0
 
 
@@ -47,12 +48,12 @@ class KMeansAlgorithm:
 
         """pack into dataframe first"""
         dframe = pd.DataFrame()
-        dframe.assign(lat=self.lat_data, long=self.long_data, pred=self.classification, idx=[i for i in range(self.n_population)])
+        dframe = dframe.assign(lat=self.lat_data, long=self.long_data, pred=self.classification, idx=[i for i in range(self.n_population)])
 
         for i in range(self.K):
             filtered_data = dframe[dframe.pred==i]
-            new_lat = self.__calculate_mean(filtered_data[["lat"]].tolist(), self.weight_factor, self.n_population)
-            new_long = self.__calculate_mean(filtered_data[["long"]].tolist(), self.weight_factor, self.n_population)
+            new_lat = self.__calculate_mean(filtered_data["lat"].tolist(), self.weight_factor, len(filtered_data))
+            new_long = self.__calculate_mean(filtered_data["long"].tolist(), self.weight_factor, len(filtered_data))
             new_centroid[i] = (new_lat, new_long)
 
         return new_centroid
@@ -95,4 +96,48 @@ class KMeansAlgorithm:
 
     def get_wcss(self):
         return self.wcss
+
+    def visualize_kmeans(self):
+        # create arrays for colors and labels based on specified K
+        colors = ["red", "green", "blue", "purple", "black", "pink", "orange"]
+        labels = ['cluster_' + str(i + 1) for i in range(self.K)]
+
+        fig1 = plt.Figure(figsize=(6,6))
+        ax1 = plt.subplot(111)
+        # plot each cluster
+        dframe = pd.DataFrame()
+        dframe = dframe.assign(lat=self.lat_data, long=self.long_data, pred=self.classification,
+                               idx=[i for i in range(self.n_population)])
+        for k in range(self.K):
+            ax1.scatter(dframe[dframe.pred==k]['long'].tolist(), dframe[dframe.pred==k]['lat'].tolist(),
+                        c=colors[k], label=labels[k])
+        # plot centroids
+        for c in self.centroid:
+            ax1.scatter(c[1], c[0],  # alpha=.5,
+                        s=300, c='lime', label='centroids')
+        plt.xlabel("lat")  # first column of df
+        plt.ylabel("long")  # second column of df
+        plt.title('Plot of K Means Clustering Algorithm')
+        plt.legend()
+
+        return plt.show(block=True)
+
+if __name__ == "__main__":
+    flname = "dataset_cluster_warehouse_exp_2.csv"
+    df = pd.read_csv(flname)
+    data_lat = []
+    data_long = []
+    for idx, row in df.iterrows():
+        point = row['lat_long'].split(',')
+        lat = float(point[0])
+        long = float(point[1])
+        data_lat.append(lat)
+        data_long.append(long)
+
+    df = df.assign(lat=data_lat, long=data_long)
+    dataset = df[['city_name', 'district_name', 'avg_demand_baseline', 'demand_target', 'lat', 'long']]
+    km = KMeansAlgorithm(dataset, 5, 'lat', 'long')
+    km.fit(100)
+    print(km.predict())
+    km.visualize_kmeans()
 
